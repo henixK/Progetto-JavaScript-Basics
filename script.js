271bb8940abf4666a0e11d94ceda01db3e7cdc9e
Kevin Bautista <kevin.bautista@outlook.it>
2023-01-05 15:17:56 +0100

    jumping charmender

--- script.js
+++ script.js
@@ -28,13 +28,22 @@ buttonContainer.append(minus, reset, plus);
 
 let countEl = document.getElementById('counter');
 let count = 0;
+let jumpHeight = 0;
+let jumpDuration = 0;
+let jumpCount = 0;
+
+const charmender = document.querySelector('.charmender');
 
 plus.addEventListener('click', function () {
     count++;
     countEl.textContent = count;
+    jumpCount++;
+    jumpHeight += 10;
 });
 
 let saved = '';
+
+
 
 reset.addEventListener('click', function () {
     saved += count + ', ';
@@ -46,4 +55,22 @@ reset.addEventListener('click', function () {
 minus.addEventListener('click', function () {
     count--;
     countEl.textContent = count;
+});
+
+
+//function jump() {
+    // change the top position of the .charmender element
+    //document.querySelector('.charmender').style.top = '20%';
+  
+    // use the transition property to animate the top position
+    // over a duration of 500ms
+    //document.querySelector('.charmender').style.transition = 'top 500ms';
+  //}
+
+  //document.querySelector('button').addEventListener('click', jump);
+
+  document.querySelectorAll('.button').forEach(button => {
+    button.addEventListener('click', () => {
+        document.querySelector('.charmender').classList.add('jump');
+    });
 });
