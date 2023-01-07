be1ef900e8fa757f86588cad9562f416819e5eee
Kevin Bautista <kevin.bautista@outlook.it>
2023-01-07 19:00:26 +0100

    added modal popup and delete saved numbers

--- js/script.js
+++ js/script.js
@@ -1,28 +1,33 @@
-function elementFromHtml(html) {
-    if (!html.trim()) {
-        return null;
-    }
-    try {
-        let template = document.createElement("template");
-        template.innerHTML = html.trim();
-        return template.content.firstElementChild;
-    } catch (error) {
-        console.error(`Error creating element from HTML string: ${error}`);
-        return null;
-    }
+//function to create multiple html elements
+function createHtmlElements(html) {
+    const template = document.createElement("template");
+    template.innerHTML = html.trim();
+    return template.content.firstElementChild
 }
 
-const buttonContainer = document.querySelector('.button-container');
-const screen = document.querySelector('.screen');
-const saveContainer = elementFromHtml("<div class='save-container'></div>");
-const savedNumbers = elementFromHtml("<span id='saved-numbers'></span>");
-const minus = elementFromHtml("<button class='minus-button'></button>");
-const reset = elementFromHtml("<button class='middle-button'></button>");
-const plus = elementFromHtml("<button class='plus-button'></button>");
-
-saveContainer.innerHTML = "Saved numbers: ";
+//Creating save section
+const screen = document.querySelector(".screen");
+const saveContainer = createHtmlElements("<div class='save-container'>Saved numbers: </div>");
+const savedNumbers = createHtmlElements("<span id='saved-numbers'></span>");
 
 
+//Creating main buttons
+const buttonContainer = document.querySelector(".button-container");
+const decrement = createHtmlElements("<button class='minus-button'></button>");
+const resetSaveDelete = createHtmlElements("<button class='resetSave-button'></button>");
+const increment = createHtmlElements("<button class='plus-button'></button>");
+
+//Creating side buttons
+const buttonSideContainer = createHtmlElements("<div class='button-side-container'></div>");
+const deleteButton = createHtmlElements("<button class='delete-button'>del</button>");
+const modalPopup = createHtmlElements("<button class='modal'>?</button>");
+
+
+const modalWindow = createHtmlElements("<div class='modal-window'></div>");
+const instructions = createHtmlElements("<p>Instructions: This is a simple counter app. Use the plus button to increment the counter, the minus button to decrement the counter, and the reset button to save the current count and reset the counter to zero. The saved numbers will be displayed in the saved numbers container. Use the delete button to delete all saved numbers. Click the modal button again or anywhere outside the modal window to close it.</p>");
+const closeButton = createHtmlElements("<button class='close-button'>&times;</button>");
+
+// Function to update the save container based on the screen size
 function updateSaveContainer() {
     if (window.matchMedia("(max-width: 480px)").matches) {
         saveContainer.innerHTML = "Saved: ";
@@ -34,19 +39,23 @@ function updateSaveContainer() {
 updateSaveContainer();
 window.addEventListener("resize", updateSaveContainer);
 
-
 saveContainer.append(savedNumbers);
-screen.append(saveContainer);
-buttonContainer.append(minus, reset, plus);
+screen.append(saveContainer, buttonSideContainer);
+buttonSideContainer.append(modalPopup, deleteButton);
+buttonContainer.append(decrement, resetSaveDelete, increment);
+modalWindow.append(closeButton, instructions);
+screen.append(modalWindow);
 
-let countEl = document.getElementById('counter');
+
+let countEl = document.getElementById("counter");
 let count = 0;
-
-const plusButton = document.querySelector(".plus-button");
 const charmender = document.querySelector(".charmender");
+const incrementButton = document.querySelector(".plus-button");
 
 
-plusButton.addEventListener('click', () => {
+
+// Add an event listener to the plus button to increment the counter
+incrementButton.addEventListener("click", () => {
     charmender.classList.add("bounce");
     count++;
     countEl.textContent = count;
@@ -54,7 +63,8 @@ plusButton.addEventListener('click', () => {
 
 let saved = '';
 
-reset.addEventListener('click', function () {
+// Add an event listener to the reset button to save the current count and reset the counter
+resetSaveDelete.addEventListener("click", function ()  {
     charmender.classList.add("bounce");
     saved += count + ', ';
     count = 0;
@@ -62,14 +72,36 @@ reset.addEventListener('click', function () {
     savedNumbers.textContent = saved;
 });
 
-minus.addEventListener('click', function () {
+deleteButton.addEventListener("click", function () {
+    saved = '';
+    savedNumbers.textContent = saved;
+});
+
+
+
+// Add an event listener to the minus button to decrement the counter
+decrement.addEventListener("click", function () {
     charmender.classList.add("bounce");
     count--;
     countEl.textContent = count;
 });
 
+
 charmender.addEventListener("animationend", () => {
     charmender.classList.remove("bounce");
 });
 
+modalPopup.addEventListener("click", function () {
+    modalWindow.classList.toggle("show-modal");
+});
 
+
+screen.addEventListener("click", function (event) {
+    if (event.target === modalWindow) {
+        modalWindow.classList.toggle("show-modal");
+    }
+});
+
+closeButton.addEventListener("click", function () {
+    modalWindow.classList.toggle("show-modal");
+});
