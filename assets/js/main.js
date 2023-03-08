b55fe2129a0508a6be56a4257b69e3f077ceaa4f
Kevin Bautista <kevin.bautista@outlook.it>
2023-03-08 18:09:01 +0100

    fixed event delegation and changed function to create elements

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,189 @@
 //function to create multiple html elements
+function createNewElement(tag, className, parent) {
+    const element = document.createElement(tag);
+    element.classList.add(className);
+    parent.appendChild(element);
+    return element;
+}
+
+const pokeball = document.querySelector(".pokeball")
+
+//Creating save section
+const screen = document.querySelector(".screen");
+const saveContainer = createNewElement("div", "save-container", screen);
+const savedNumbers = createNewElement("span", "saved-numbers", saveContainer);
+savedNumbers.textContent = "Saved numbers: ";
+
+// Creating div wrapper for buttons 
+const buttonWrapper = createNewElement("div", "button-wrapper", pokeball);
+
+//Creating main buttons
+const buttonContainer = createNewElement("div", "button-container", buttonWrapper)
+createNewElement("button", "decrement-button", buttonContainer);
+createNewElement("button", "resetSave-button", buttonContainer);
+createNewElement("button", "increment-button", buttonContainer);
+
+//Creating side buttons
+const buttonSideContainer = createNewElement("div", "button-side-container", buttonWrapper);
+createNewElement("button", "modal", buttonSideContainer).textContent= "?";
+createNewElement("button", "delete-button", buttonSideContainer).textContent= "del";
+
+//Creating the Popup instructions
+const modalWindow = createNewElement("div", "modal-window", screen);
+const closeButton = createNewElement("button", "close-button", modalWindow);
+closeButton.textContent = "x";
+const instructions = createNewElement("p", null, modalWindow);
+instructions.textContent = "Instructions: This is a simple counting application. Use the button on the right to increment the counter, the one on the left to decrement it, and the one in the center to save the current count and reset the counter. The saved numbers will be displayed in the 'Saved numbers' section. Use the 'delete' button to remove all saved numbers.";
+
+let countEl = document.getElementById("counter");
+let count = 0;
+let saved = '';
+const charmender = document.querySelector(".charmender");
+
+//Event Delegation for buttons
+buttonWrapper.addEventListener("click", event => {
+    const targetClass = event.target.classList;
+    if (targetClass.contains("decrement-button")) {
+        count--;
+    } else if (targetClass.contains("resetSave-button")) {
+        saved += count + ', ';
+        count = 0;
+    } else if (targetClass.contains("increment-button")) {
+        count++;
+    } else if (targetClass.contains("modal")) {
+        modalWindow.classList.toggle("show-modal");
+    } else if (targetClass.contains("delete-button")) {
+        saved = '';
+    }
+    charmender.classList.add("bounce");
+    countEl.textContent = count;
+    savedNumbers.textContent = "Saved numbers: " + saved;
+});
+
+closeButton.addEventListener("click", function () {
+    modalWindow.classList.toggle("show-modal");
+});
+
+charmender.addEventListener("animationend", () => {
+    charmender.classList.remove("bounce");
+});
+
+// Function to update the save container based on the screen size
+function updateSaveContainer() {
+    if (window.matchMedia("(max-width: 480px)").matches) {
+        saveContainer.innerHTML = "Saved: ";
+    } else {
+        saveContainer.innerHTML = "Saved numbers: ";
+    }
+}
+
+updateSaveContainer();
+window.addEventListener("resize", updateSaveContainer);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* 
+
+
+// Funzione per creare un nuovo elemento HTML e aggiungerlo a un nodo genitore
+function createNewElement(tag, className, parent) {
+    const element = document.createElement(tag);
+    element.classList.add(className);
+    parent.appendChild(element);
+    return element;
+}
+
+
+// Creazione sezione salvataggio
+const screen = document.querySelector(".screen");
+const saveContainer = createNewElement("div", "save-container", screen);
+createNewElement("span", "saved-numbers", saveContainer);
+
+// Creazione pulsanti principali
+const buttonContainer = document.querySelector(".button-container");
+createNewElement("button", "minus-button", buttonContainer);
+createNewElement("button", "resetSave-button", buttonContainer);
+createNewElement("button", "plus-button", buttonContainer);
+
+// Creazione pulsanti laterali
+const buttonSideContainer = createNewElement("div", "button-side-container", buttonsContainer);
+createNewElement("button", "modal", buttonSideContainer);
+createNewElement("button", "delete-button", buttonSideContainer);
+
+// Creazione wrapper bottoni
+const buttonMain = createNewElement("div", null, pokeball)
+
+// Creazione delle istruzioni Popup
+const modalWindow = createNewElement("div", "modal-window", screen);
+const closeButton = createNewElement("button", "close-button", modalWindow);
+const instructions = createNewElement("p", null, modalWindow);
+instructions.textContent = "Istruzioni:\\Questo è un'applicazione di conteggio semplice.\\Usa il pulsante a destra per incrementare il contatore, quello a sinistra per decrementarlo e quello al centro per salvare il conteggio attuale e azzerare il contatore.\\I numeri salvati saranno visualizzati nella sezione 'Saved numbers'.\\Usa il pulsante 'delete' per eliminare tutti i numeri salvati.";
+
+
+let countEl = document.getElementById("counter");
+let count = 0;
+let saved = '';
+const charmender = document.querySelector(".charmender");
+
+// Aggiunta listener agli eventi con Event Delegation
+screen.addEventListener("click", event => {
+    const targetClass = event.target.classList;
+    if (targetClass.contains("minus-button")) {
+        count--;
+    } else if (targetClass.contains("resetSave-button")) {
+        saved += count + ', ';
+        count = 0;
+    } else if (targetClass.contains("plus-button")) {
+        count++;
+    } else if (targetClass.contains("modal")) {
+        modalWindow.classList.toggle("show-modal");
+    } else if (targetClass.contains("delete-button")) {
+        saved = '';
+    }
+    charmender.classList.add("bounce");
+    countEl.textContent = count;
+    saveContainer.querySelector(".saved-numbers").textContent = saved;
+});
+
+closeButton.addEventListener("click", function () {
+    modalWindow.classList.toggle("show-modal");
+});
+
+charmender.addEventListener("animationend", () => {
+    charmender.classList.remove("bounce");
+});
+
+// Funzione per aggiornare la sezione salvataggio in base alla dimensione dello schermo
+function updateSaveContainer() {
+    if (window.matchMedia("(max-width: 480px)").matches) {
+        saveContainer.innerHTML = "Salvati: ";
+    } else {
+        saveContainer.innerHTML = "Numeri salvati: ";
+    }
+}
+
+updateSaveContainer();
+window.addEventListener("resize", updateSaveContainer); */
+
+/* //function to create multiple html elements
 function createHtmlElements(html) {
     const template = document.createElement("template");
     template.innerHTML = html.trim();
@@ -81,6 +266,4 @@ closeButton.addEventListener("click", function () {
 
 charmender.addEventListener("animationend", () => {
     charmender.classList.remove("bounce");
-});
-
-
+}); */
